b1b9b28008e248d8d2a5dded717b0b41ad17d27b
fahim islam <mdfahimislam3244@gmail.com>
2024-01-16 18:29:17 +0600

    google signUp added

--- src/Pages/signUp/SignUp.jsx
+++ src/Pages/signUp/SignUp.jsx
@@ -1,12 +1,14 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Swal from "sweetalert2";
 import { AuthContext } from "../../provider/AuthProvider";
-
+import { AiFillGoogleCircle } from "react-icons/ai";
 
 const SignUp = () => {
-    const { createUser } = useContext(AuthContext);
+    const { createUser,signInWithGoogle } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const handelSignUp = (e) => {
         e.preventDefault();
@@ -97,12 +99,23 @@ const SignUp = () => {
                 console.error(error);
             });
     };
+
+    const handelWithGoogle = () => {
+        signInWithGoogle()
+            .then((result) => {
+                console.log(result.user);
+                navigate(location?.state ? location.state : "/");
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
     return (
         <div className="w-[100%] mt-20 mb-10 ">
             <div className="w-[80%]  flex flex-col gap-10 lg:flex-row mx-auto">
                 <div className=" flex-1 w-full ">
                     <div className="text-center ">
-                        <h1 className=" text-6xl font-bold  py-3">Welcome to</h1>
+                        <h1 className=" text-6xl font-bold  pt-36">Welcome to</h1>
                         <div className="divider w-[30%] mx-auto"></div>
                         <p className="text-xl font-normal">
                              Mega Market! 🌟 Explore, connect, and enjoy a seamless experience. Your journey with us begins now. For assistance, contact our support team. Thrilled to have you on board!!</p>
@@ -132,8 +145,17 @@ const SignUp = () => {
                                 <button className="btn btn-primary">Sign Up</button>
                             </div>
                         </form>
+                        <div className="divider text-black">OR</div>
+                            <div className="flex gap-2 items-center justify-center">
+                                <button onClick={handelWithGoogle}>
+                                    {" "}
+                                    <AiFillGoogleCircle className="text-3xl text-red-400">
+                                        google
+                                    </AiFillGoogleCircle>
+                                </button>
+                            </div>
                         <p className="text-center pt-6">
-                            New here?please {""}
+                        Already have account?please {""} {""}
                             <Link to="/signIn" className=" text-rose-400 font-bold">
                                 SignIn
                             </Link>
