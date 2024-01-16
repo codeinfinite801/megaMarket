671bc9b4d4c0501b9d1b2e5e55cff3c79aae6de0
fahim islam <mdfahimislam3244@gmail.com>
2024-01-16 16:07:34 +0600

    signIn page added

--- src/Routs/Routs.jsx
+++ src/Routs/Routs.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home";
+import SignIn from "../Pages/signIn/SignIn";
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/signIn',
+                element: <SignIn></SignIn>
             },
         ]
     },
