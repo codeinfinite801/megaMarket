da0c24b1ad73df005f75f08c6cdee075b01928f9
Badhon-Roy <roybadhon286@gmail.com>
2024-01-18 15:41:43 +0600

    problem solved

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
                             <ul tabIndex={1} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                                 <li>
                                     <a className="justify-between">
-                                        Profil
+                                        Profile
                                     </a>
                                 </li>
                                 <li><a>Settings</a></li>
