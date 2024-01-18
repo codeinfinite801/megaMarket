27c89a94382db74a31b1233c47a462db9443e091
Sakib Hossain] <sakib01181@gmail.com>
2024-01-18 18:33:37 +0600

    2 section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,17 @@
+import AuthorCategory from "./AuthorCategory/AuthorCategory";
 import Banner from "./Banner/Banner";
+import SuperStore from "./SuperStore/SuperStore";
+
 
 
 const Home = () => {
     return (
-        <div>
+        <div className="bg-[#cc00ff21]">
             <Banner></Banner>
+            <div className="mx-5">
+                <SuperStore></SuperStore>
+                <AuthorCategory></AuthorCategory>
+            </div>
         </div>
     );
 };
