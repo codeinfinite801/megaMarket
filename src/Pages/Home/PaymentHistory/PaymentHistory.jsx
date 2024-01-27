71eabc6fb128fe45c67fb956ec288bad26346a41
Badhon-Roy <roybadhon286@gmail.com>
2024-01-27 18:57:56 +0600

    payment history page created

--- src/Pages/Home/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/Home/PaymentHistory/PaymentHistory.jsx
@@ -1,8 +1,25 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import History from "./History";
 
 const PaymentHistory = () => {
+    const {user} = useContext(AuthContext)
+    const {data , refetch } = useQuery({
+        queryKey: ['paymentHistory', user?.email],
+        queryFn: async () =>{
+            const res = await axios.get(`https://maga-market-server-eta.vercel.app/payments?email=${user.email}`)
+            return res.data
+        }
+
+    })
     return (
-        <div className="my-16">
-            <h2 className="text-4xl font-bold text-center my-4">Payment History</h2>
+        <div className="my-8 mx-4">
+            <h2 className="md:text-3xl text-xl font-bold mb-8 text-center">Payment History</h2>
+            {
+                data?.map(history => <History key={history?._id} history={history}></History>)
+            }
         </div>
     );
 };
