b5e2c692087919246e2ec94bac26547a8142dbff
SuronjoyRoy <suronjoyroy21@gmail.com>
2024-02-20 15:21:59 +0600

    [feat](Recently sold):add best sellers product

--- src/BestSeller/BestSeller.jsx
+++ src/BestSeller/BestSeller.jsx
@@ -1,23 +1,123 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+// import SingleSell from "./SingleSell";
+// import Popular from "../Pages/PlaceOrder/Popular";
+
+import { useState } from "react";
+import Popular from "../Pages/PlaceOrder/Popular";
+
 
 const BestSeller = () => {
-    const [bestSell, setBestSell] = useState([]);
+    // const [bestSell, setBestSell] = useState([]);
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(data => setBestSell(data))
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://maga-market-server-eta.vercel.app/kidsZone')
+    //         .then(res => res.json())
+    //         .then(data => setBestSell(data))
+    // }, [])
+
+    // function Tabs() {
+    const [activeTab, setActiveTab] = useState('#styled-profile');
+
+    const handleTabClick = (tabId) => {
+        setActiveTab(tabId);
+    };
 
 
     return (
-        <div className="py-20">
-            <h1>total user:{bestSell.length}</h1>
-            {
-                bestSell.map(sells => <li>{sells.name}</li>)
-            }
+        <div className="py-20 bg-white">
+            <div>
+                <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
+                    <ul className="flex flex-wrap -mb-px text-sm font-medium text-center justify-center" id="default-styled-tab" role="tablist">
+                        <li className="me-2" role="presentation">
+                            <button
+                                className={`inline-block p-4 border-b-2 text-3xl rounded-t-lg ${activeTab === '#styled-profile' ? 'text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500' : 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300 text-3xl'}`}
+                                id="profile-styled-tab"
+                                onClick={() => handleTabClick('#styled-profile')}
+                                type="button"
+                                role="tab"
+                                aria-controls="profile"
+                                aria-selected={activeTab === '#styled-profile'}
+                            >
+                                Best Sellers
+                            </button>
+                        </li>
+                        <li className="me-2" role="presentation">
+                            <button
+                                className={`inline-block p-4 border-b-2 text-3xl rounded-t-lg ${activeTab === '#styled-dashboard' ? 'text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500' : 'hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-3xl'}`}
+                                id="dashboard-styled-tab"
+                                onClick={() => handleTabClick('#styled-dashboard')}
+                                type="button"
+                                role="tab"
+                                aria-controls="dashboard"
+                                aria-selected={activeTab === '#styled-dashboard'}
+                            >
+                                Featured Products
+                            </button>
+                        </li>
+
+                    </ul>
+                    <div id="default-styled-tab-content">
+                        <div
+                            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${activeTab === '#styled-profile' ? '' : 'hidden'}`}
+                            id="styled-profile"
+                            role="tabpanel"
+                            aria-labelledby="profile-tab"
+                        >
+                            <Popular></Popular>
+                        </div>
+                        <div
+                            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${activeTab === '#styled-dashboard' ? '' : 'hidden'}`}
+                            id="styled-dashboard"
+                            role="tabpanel"
+                            aria-labelledby="dashboard-tab"
+                        >
+                            <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                        </div>
+                        <div
+                            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${activeTab === '#styled-settings' ? '' : 'hidden'}`}
+                            id="styled-settings"
+                            role="tabpanel"
+                            aria-labelledby="settings-tab"
+                        >
+                            <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                        </div>
+                        <div
+                            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${activeTab === '#styled-contacts' ? '' : 'hidden'}`}
+                            id="styled-contacts"
+                            role="tabpanel"
+                            aria-labelledby="contacts-tab"
+                        >
+                            <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            {/* <div className="">
+                <div role="tablist" className="tabs tabs-bordered flex justify-center items-center">
+                    <div>
+                        <input type="radio" name="my_tabs_1" role="tab" className="tab text-3xl" aria-label="Best Sellers" />
+                        <div role="tabpanel" className="tab-content p-10">
+                            <Popular></Popular>
+                        </div>
+
+                    </div>
+                    <div>
+                        <input type="radio" name="my_tabs_1" role="tab" className="tab text-3xl" aria-label="Featured Products" checked />
+                        <div role="tabpanel" className="tab-content p-10">Featured Products</div>
+                    </div>
+                </div>
+            </div> */}
+            {/* <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2  gap-3">
+                {
+                    bestSell?.slice(5, 10)?.map(sells => <SingleSell key={sells?._id} sells={sells}></SingleSell>)
+                }
+            </div> */}
         </div>
     );
-};
+}
 
 export default BestSeller;
--- src/Pages/Home/KidsZones/KidsZones.jsx
+++ src/Pages/Home/KidsZones/KidsZones.jsx
@@ -4,18 +4,18 @@ import KidZone from "../KidZone/KidZone";
 
 
 const KidsZones = () => {
-    const {category} = useParams();
+    const { category } = useParams();
     const [kids, setKids] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`https://maga-market-server-eta.vercel.app/kidsZone?category=${category}`)
-        .then(res => res.json())
-        .then(data =>setKids(data))
-    },[category])
+            .then(res => res.json())
+            .then(data => setKids(data))
+    }, [category])
 
     // const { data, refetch } = useBooks({ category })
     const [brand, setBrand] = useState([])
-    
+
     useEffect(() => {
         if (kids) {
             const brandList = kids.reduce((acc, kid) => {
@@ -30,65 +30,65 @@ const KidsZones = () => {
     }, [kids]);
     return (
         <div className="px-5 my-16">
-        <div className="grid grid-cols-12 gap-2">
-            <div className="lg:col-span-2 md:col-span-4 col-span-12">
-                <div className="bg-white shadow-xl rounded-lg p-3 border">
-                    <div className="py-2 px-2 border-b-2">
-                        <p className="font-bold">Sort</p>
+            <div className="grid grid-cols-12 gap-2">
+                <div className="lg:col-span-2 md:col-span-4 col-span-12">
+                    <div className="bg-white shadow-xl rounded-lg p-3 border">
+                        <div className="py-2 px-2 border-b-2">
+                            <p className="font-bold">Sort</p>
+                        </div>
+                        <div className="flex items-center justify-center gap-4 my-2">
+                            <input type="radio" name="sort" id="best-seller" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                            <label htmlFor="best-seller" className="ml-2 block text-sm font-medium text-gray-700 flex-1">Best Seller</label>
+                        </div>
+                        <div className="flex items-center justify-center gap-4 my-2">
+                            <input type="radio" name="sort" id="low-to-high" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                            <label htmlFor="low-to-high" className="ml-2 block text-sm font-medium text-gray-700 flex-1">Price Low to High</label>
+                        </div>
+                        <div className="flex items-center justify-center gap-4 my-2">
+                            <input type="radio" name="sort" id="high-to-low" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                            <label htmlFor="high-to-low" className="ml-2 block text-sm font-medium text-gray-700 flex-1">Price High to Low</label>
+                        </div>
+                        <div className="flex items-center justify-center gap-4 my-2">
+                            <input type="radio" name="sort" id="discount" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                            <label htmlFor="discount" className="ml-2 block text-sm font-medium text-gray-700 flex-1">Discount</label>
+                        </div>
                     </div>
-                    <div className="flex items-center justify-center gap-4 my-2">
-                        <input type="radio" name="sort" id="best-seller" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                        <label htmlFor="best-seller" className="ml-2 block text-sm font-medium text-gray-700 flex-1">Best Seller</label>
+                    {/* sort - 2 */}
+                    <div>
+                        <div className="flex items-center justify-center gap-4 shadow-xl border-2 px-2 py-4 my-2">
+                            <input type="radio" name="sort" id="inStock" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                            <label htmlFor="discount" className="ml-2 block text-sm font-medium text-gray-700 flex-1">In Stock</label>
+                        </div>
                     </div>
-                    <div className="flex items-center justify-center gap-4 my-2">
-                        <input type="radio" name="sort" id="low-to-high" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                        <label htmlFor="low-to-high" className="ml-2 block text-sm font-medium text-gray-700 flex-1">Price Low to High</label>
-                    </div>
-                    <div className="flex items-center justify-center gap-4 my-2">
-                        <input type="radio" name="sort" id="high-to-low" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                        <label htmlFor="high-to-low" className="ml-2 block text-sm font-medium text-gray-700 flex-1">Price High to Low</label>
-                    </div>
-                    <div className="flex items-center justify-center gap-4 my-2">
-                        <input type="radio" name="sort" id="discount" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                        <label htmlFor="discount" className="ml-2 block text-sm font-medium text-gray-700 flex-1">Discount</label>
-                    </div>
-                </div>
-                {/* sort - 2 */}
-                <div>
-                    <div className="flex items-center justify-center gap-4 shadow-xl border-2 px-2 py-4 my-2">
-                        <input type="radio" name="sort" id="inStock" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                        <label htmlFor="discount" className="ml-2 block text-sm font-medium text-gray-700 flex-1">In Stock</label>
-                    </div>
-                </div>
-                {/* Filter By Brand */}
-                <div className="bg-white shadow-xl rounded-lg p-3 border">
-                    <div className="py-2 px-2 border-b-2">
-                        <p className="font-bold">Filter By Brand</p>
-                    </div>
-                    {
-                        brand?.map((item, index) => {
-                            return <div key={index}>
-                                <div className="flex items-center justify-center gap-4 my-2">
-                                    <input type="checkbox" name="sort" id="best-seller" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                                    <label htmlFor="best-seller" className="ml-2 block text-sm font-medium text-gray-700 flex-1">{item}</label>
+                    {/* Filter By Brand */}
+                    <div className="bg-white shadow-xl rounded-lg p-3 border">
+                        <div className="py-2 px-2 border-b-2">
+                            <p className="font-bold">Filter By Brand</p>
+                        </div>
+                        {
+                            brand?.map((item, index) => {
+                                return <div key={index}>
+                                    <div className="flex items-center justify-center gap-4 my-2">
+                                        <input type="checkbox" name="sort" id="best-seller" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                                        <label htmlFor="best-seller" className="ml-2 block text-sm font-medium text-gray-700 flex-1">{item}</label>
+                                    </div>
                                 </div>
-                            </div>
-                        })
-                    }
+                            })
+                        }
+
+                    </div>
 
                 </div>
-
-            </div>
-            <div className="lg:col-span-10 md:col-span-8 col-span-12">
-                <h2 className="text-2xl mb-5">{category} {kids?.length}</h2>
-                <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2  gap-3">
-                    {
-                        kids?.map(kid => <KidZone key={kid?._id} kid={kid}></KidZone>)
-                    }
+                <div className="lg:col-span-10 md:col-span-8 col-span-12">
+                    <h2 className="text-2xl mb-5">{category} {kids?.length}</h2>
+                    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2  gap-3">
+                        {
+                            kids?.map(kid => <KidZone key={kid?._id} kid={kid}></KidZone>)
+                        }
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 
