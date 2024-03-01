fa891a85f09a22d4f05d8833fd810c4283af3913
Sakib mia <sakib01181@gmail.com>
2024-03-01 09:21:26 +0600

    superstore redesigned

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -60,28 +60,28 @@ const Category = () => {
                         <div className="swiper-button-next invisible lg:visible"></div>
                     </Swiper>
                 </div> */}
-                <div className="w-full pl-4 pr-2">
-                    <div className="grid items-center gap-5 grid-cols-12">
-                        <div className="col-span-4">
+                <div className="w-full lg:pl-4 lg:pr-2 py-4">
+                    <div className="lg:grid md:grid items-center gap-5 grid-cols-12">
+                        <div className="col-span-4 mx-2 mb-2">
                             {categories.slice(0, 1).map((item, index) => (
                                 <div key={index} className="transition-transform duration-300 transform hover:scale-105">
                                     <Link to={`/allBooks/${item?.category}`} className="block">
-                                        <div className="bg-gray-300 flex flex-col items-center justify-center h-[425px] rounded-lg">
-                                            <img className='rounded-xl w-5/6 py-5 h-62 px-5 mx-auto' src={item?.image} alt={item?.name} />
-                                            <h1 className="text-base mt-5 p-5">{item?.category}</h1>
+                                        <div className="bg-gray-300 flex lg:flex-col flex-row-reverse items-end lg:items-center justify-center lg:h-[425px] md:h-72 rounded-lg">
+                                            <img className='rounded-xl lg:w-5/6 md:1/2 py-5 lg:h-62 px-5 mx-auto' src={item?.image} alt={item?.name} />
+                                            <h1 className="text-base md:text-sm mt-5 p-5">{item?.category}</h1>
                                         </div>
                                     </Link>
                                 </div>
                             ))}
                         </div>
-                        <div className="col-span-8">
-                            <div className="grid grid-cols-2 gap-10 rounded m-2">
+                        <div className="col-span-8 mx-2">
+                            <div className="lg:grid w-full md:grid grid-cols-2 lg:gap-10 space-y-2 md:gap-5 rounded lg:m-2">
                                 {categories.slice(1, 5).map((item, index) => (
                                     <div key={index} className="transition-transform duration-300 transform hover:scale-105">
                                         <Link to={`/allBooks/${item?.category}`} className="block">
                                             <div className="bg-gray-300 rounded-lg flex items-end">
-                                                <h1 className="text-base mt-5 p-5">{item?.category}</h1>
-                                                <img className='rounded-xl w-3/6 py-5 h-48 px-5 mx-auto' src={item?.image} alt={item?.name} />
+                                                <h1 className="text-base md:text-sm mt-5 p-5">{item?.category}</h1>
+                                                <img className='rounded-xl lg:w-3/6 md:w-1/2 py-5 lg:h-48 md:h-32 px-5 mx-auto' src={item?.image} alt={item?.name} />
                                             </div>
                                         </Link>
                                     </div>
