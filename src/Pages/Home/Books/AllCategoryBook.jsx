567b8bdaec5b2325bf5f22347c398360d53d96fe
Sakib mia <sakib01181@gmail.com>
2024-03-01 16:49:05 +0600

    all category comp added

--- src/Pages/Home/Books/AllCategoryBook.jsx
+++ src/Pages/Home/Books/AllCategoryBook.jsx
@@ -11,25 +11,33 @@ const AllCategoryBook = () => {
             })
     }, [])
     return (
+
         <div className="bg-white my-5 px-5">
-            <div className="flex items-center justify-between my-5 pl-2 text-xl border-2 border-l-red-800">
-                <h1>All Category Books</h1>
+            <div className="flex items-center justify-between my-5 pl-2 text-xl border-l-4 border-l-red-800">
+                <h1 className="font-bold">All Category Books</h1>
             </div>
-            <div className="grid grid-cols-5 gap-5 items-center justify-center" >
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 items-center justify-center">
                 {
                     data?.map(book => {
-                        return <div key={book?._id}>
-                            <Link to={`/allbooks/${book?._id}`}>
-                                <div>
-                                    <img className="h-72 rounded" src={book?.image} alt="" />
-                                    <h1 className="mt-4 text-lg pb-3">{book?.name}</h1>
-                                </div>
-                            </Link>
-                        </div>
+                        return (
+                            <div key={book?._id} className="bg-gray-100 rounded-lg overflow-hidden hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                                <Link to={`/allbooks/${book?.category}`} className="block">
+                                    <div className="relative">
+                                        <img className=" w-full" src={book?.image} alt={book?.name} />
+                                        <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
+                                            <h1 className="text-lg font-semibold text-white text-center">{book?.name}</h1>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        )
                     })
                 }
             </div>
         </div>
+
+
+
     );
 };
 
